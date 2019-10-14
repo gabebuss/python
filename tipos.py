18a7cae493abbf15bb205d8711168d2f87cf2d69
gabriel <gabebuss@gmail.com>
2019-10-13 21:23:44 -0300

    estudo python

---
+++ tipos.py
@@ -0,0 +1,34 @@
+# comentario em uma linha
+
+""" comentario
+em 2 linhas
+"""
+
+print("Meu primeiro codigo em python\n")
+
+# criar uma variavel
+nome_da_variavel = "Valor_da_variavel"
+
+#string
+nome = "Gabriel"
+sobrenome = 'Buss'
+
+#int, float
+inteiro = 10
+flutuante = 1.0
+numero = "10" #string
+pythonelegal = True #com maiscula
+ptyhon_e_dificil = False
+
+#calculo
+soma = 3 + 3
+subtracao = 3 - 3
+divisao = 3 / 3
+multiplicacao = 3 * 3
+igual = 3 == 3
+diferente = 3 != 2
+menor = 3 < 2
+maior = 3 > 2
+
+print(nome, sobrenome)
+
