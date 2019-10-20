c083c6c6b90b182ecee1255dd843972488ab7ea8
gabriel <gabebuss@gmail.com>
2019-10-20 20:48:16 -0300

    estudo python

---
+++ prints.py
@@ -0,0 +1,29 @@
+time1 = "santos"
+time2 = "palmeiras"
+
+print(time1, time2)
+
+print(time1 + time2)
+
+#python2
+print("O %s nao tem mundial!!!" % time2)
+print("vai pra cima deles %s" % time1)
+print("o %s é maior que o %s!!!" % (time1, time2) )
+
+#python3
+print("O {} time1 nao tem mundial!!!".format(time2) )
+print("O {1} eh maior que o {}!!!".format(time1, time2) )
+
+#imput
+time = input(" Qual o seu nome: ")
+print("o nome digitado foi {}". format(time))
+
+#tipos
+inteiro = 1
+flutuante = 1.0
+print (inteiro)
+print (flutuante)
+print (inteiro)
+
+print( type( float(inteiro)))
+print ()
